32917a4d7fd2b5458322b4a4585b41bf130da758
Tomáš Hejátko <tomas.hejatko@gmail.com>
2019-03-14 13:03:36 +0100

    Cosmetic changes

--- packer/stackdriver.sh
+++ packer/stackdriver.sh
@@ -6,8 +6,7 @@ curl --connect-timeout 5 -s -f "https://packages.cloud.google.com/apt/doc/apt-ke
 sudo apt-get -qq update
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y -q install stackdriver-agent google-fluentd google-fluentd-catch-all-config
 DEBIAN_FRONTEND=noninteractive sudo apt-get clean
-sudo rm -Rf /var/lib/apt/*
-
+sudo rm -Rf /var/lib/apt/lists/*
 
 cd /opt/stackdriver/collectd/etc/collectd.d/ 
 sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/redis.conf
